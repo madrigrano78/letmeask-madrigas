a6a775e73c6c6f7a908d24ccd1df00a865d6e599
Gabriel Madrigrano <gabrielmadrigranorodrigues@gmail.com>
2022-02-16 20:53:33 -0300

    Projeto Madrigas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-function App() {
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import { Home } from "./pages/Home";
+import NewRoom from "./pages/NewRoom";
+import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
+export function App() {
   return (
-    <h1>Mundo Madroga</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms/new" element={<NewRoom />} />
+          <Route path="/rooms/:id" element={<Room />} />
+          <Route path="/admin/rooms/:id" element={<AdminRoom />} />
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
-export default App;
+
